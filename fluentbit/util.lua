3dfbb5964e831d1556386c0a0f304fb60f9caa92
Krishan Patel <krishan711@gmail.com>
2025-08-21 13:41:15 +0100

    Fiddle: update versions and add ys-appbox + disable for others (#7)
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .

--- fluentbit/util.lua
+++ fluentbit/util.lua
@@ -3,3 +3,16 @@ function append_tag(tag, timestamp, record)
     newRecord["tag"] = tag
     return 2, timestamp, newRecord
 end
+
+function extract_container_id(tag, timestamp, record)
+    newRecord = record
+    if record["containerLogPath"] then
+        local path = record["containerLogPath"]
+        local container_id = string.match(path, "/var/lib/docker/containers/([^/]+)/")
+        if container_id then
+            newRecord["containerId"] = container_id
+        end
+        newRecord["containerLogPath"] = nil
+    end
+    return 2, timestamp, newRecord
+end
